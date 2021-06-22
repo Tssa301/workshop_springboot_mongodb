6bc79d3af51e99343c3b71da587478da147a154a
Tiago Ssa <tiago.silva301@gmail.com>
2021-06-22 16:44:54 +0100

    Referencing user posts

--- src/main/java/com/tiagossa301/workshopmongo/config/Instantiation.java
+++ src/main/java/com/tiagossa301/workshopmongo/config/Instantiation.java
@@ -42,5 +42,8 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("22/06/2021"), "Good morning!", "I'm so happy today ;)", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
