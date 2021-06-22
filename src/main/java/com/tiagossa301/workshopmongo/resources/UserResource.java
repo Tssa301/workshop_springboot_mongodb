424c71201c830cf5bdeffdca5bb2654455d6095e
Tiago Ssa <tiago.silva301@gmail.com>
2021-06-22 17:39:48 +0100

    Getting a post by id

--- src/main/java/com/tiagossa301/workshopmongo/resources/UserResource.java
+++ src/main/java/com/tiagossa301/workshopmongo/resources/UserResource.java
@@ -33,7 +33,7 @@ public class UserResource {
 	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<UserDTO> findAll(@PathVariable String id) {
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
---
+++ src/main/java/com/tiagossa301/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.tiagossa301.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tiagossa301.workshopmongo.domain.Post;
+import com.tiagossa301.workshopmongo.repository.PostRepository;
+import com.tiagossa301.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
